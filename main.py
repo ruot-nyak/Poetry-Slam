6a4fb30c12f761504ee2c4b0948189c60e20790f
ruot-nyak <nyak.g.ruot@gmail.com>
2020-10-06 18:11:06 -0700

    print poem backwards

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ def get_file_lines(filename):
     lines = in_file.readlines()
     in_file.close()
     return lines 
+
+def lines_printed_backwards(lines_list):
+    lines_list.reverse()
+    lines_length = len(lines_list)
+    for i in range(lines_length):
+        line = lines_list[i]
+        line_num = lines_length - i
+        print(f"{line_num} {line}")
+ 
